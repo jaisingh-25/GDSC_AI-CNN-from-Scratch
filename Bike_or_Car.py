0caff8011b07fbd91e77e4e131a59c36119471be
jaisingh-25 <113584679+jaisingh-25@users.noreply.github.com>
2023-08-05 21:03:51 +0530

    Update Bike_or_Car.py

--- Bike_or_Car.py
+++ Bike_or_Car.py
@@ -22,9 +22,7 @@ train_datagen=ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2
                                  horizontal_flip=True)
 test_datagen=ImageDataGenerator(rescale=1./255)
 
-training_set=train_datagen.flow_from_directory('C:/Users/jaisi/Documents/Jai/Programs/College/CNN/dataset/Train',
-                                               target_size=(64,64), batch_size=20,
-                                               class_mode='binary')
+training_set=train_datagen.flow_from_directory('.../dataset/Train', target_size=(64,64), batch_size=20, class_mode='binary')
 
 test_set=test_datagen.flow_from_directory('C:/Users/jaisi/Documents/Jai/Programs/College/CNN/dataset/Test',
                                                target_size=(64,64), batch_size=20,
