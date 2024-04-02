60c2871338c612871e7247da9f41fd9a5eff8fa3
jaisingh-25 <113584679+jaisingh-25@users.noreply.github.com>
2024-04-02 23:44:43 +0530

    Update Bike_or_Car.py

--- Bike_or_Car.py
+++ Bike_or_Car.py
@@ -5,7 +5,6 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 from keras.preprocessing.image import ImageDataGenerator
-
 # Convolution, Pooling, Flattening, Dense
 classifier = Sequential()
 classifier.add(Conv2D(32, (3,3), input_shape = (64, 64, 3), activation='relu'))
