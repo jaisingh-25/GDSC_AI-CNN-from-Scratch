5da12ee7c9f9d33773ecf913888121ab84574540
jaisingh-25 <113584679+jaisingh-25@users.noreply.github.com>
2024-04-05 00:04:57 +0530

    Update Bike_or_Car.py

--- Bike_or_Car.py
+++ Bike_or_Car.py
@@ -30,7 +30,6 @@ test_image=image.img_to_array(test_image)
 test_image=np.expand_dims(test_image, axis=0)
 result=classifier.predict(test_image)
 training_set.class_indices
-
 if result[0][0]==1:
     prediction='car'
 else:
