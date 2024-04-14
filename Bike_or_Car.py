6605aeb5ecadcea5baebbea27804225c1e0b8ebc
jaisingh-25 <113584679+jaisingh-25@users.noreply.github.com>
2024-04-14 17:44:54 +0530

    Update Bike_or_Car.py

--- Bike_or_Car.py
+++ Bike_or_Car.py
@@ -19,8 +19,7 @@ train_datagen=ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2
 test_datagen=ImageDataGenerator(rescale=1./255)
 training_set=train_datagen.flow_from_directory('.../dataset/Train', target_size=(64,64), batch_size=20, class_mode='binary')
 test_set=test_datagen.flow_from_directory('.../dataset/Test', target_size=(64,64), batch_size=20, class_mode='binary')
-classifier.fit(training_set, steps_per_epoch=10, epochs=10, validation_data=test_set,
-                         validation_steps=1)
+classifier.fit(training_set, steps_per_epoch=10, epochs=10, validation_data=test_set, validation_steps=1)
 # Classification example with CNN and prediction
 import numpy as np
 from tensorflow.keras.preprocessing import image
